404c03c9399dac1f879840cce3c974292a3b53eb
boomermath <63861512+boomermath@users.noreply.github.com>
2020-09-21 11:53:09 -0400

    Create fogconfig.js

---
+++ backjs/fogconfig.js
@@ -0,0 +1,12 @@
+VANTA.FOG({
+  el: "#your-element-selector",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  highlightColor: 0x898989,
+  midtoneColor: 0x0,
+  lowlightColor: 0x69ff,
+  baseColor: 0x0
+})
